5396b67ca31768902a4b7834926e8fc3aa4ea65b
Kevin <herrbach.kevin@gmail.com>
2023-11-26 05:18:01 +0900

    Retreves the list of symptoms for an hospital in the search

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,14 @@ const pool = require('../db/db')
 router.post('/', (req, res) => {
   const symptoms = "'" + req.body.symptoms.join("','") + "'"
   const english = req.body.englishOnly
-  let query = `SELECT h.name, h.long, h.lat, COUNT(DISTINCT s.id) as matched_symptom_count FROM hospital as h
+  let query = `SELECT h.name, h.long, h.lat, 
+  (SELECT string_agg(s2.name, ',') FROM hospital h2 
+    INNER JOIN doctor as d2 on d2.hospital_id = h2.id
+    INNER JOIN doctor_illness as di2 on di2.doctor_id = d2.id
+    INNER JOIN illness as i2 on i2.id = di2.illness_id
+    INNER JOIN illness_symptom as ils2 on ils2.illness_id = i2.id
+    INNER JOIN symptom s2 on ils2.symptom_id = s2.id where h2.id = h.id) as symptoms,
+  COUNT(DISTINCT s.id) as matched_symptom_count FROM hospital as h
   INNER JOIN doctor as d on d.hospital_id = h.id
   INNER JOIN doctor_illness as di on di.doctor_id = d.id
   INNER JOIN illness as i on i.id = di.illness_id
